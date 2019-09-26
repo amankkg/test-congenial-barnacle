f98dae2555f768c3f30eea7e64f370ef63476f4d
Aman Kubanychbek <amankubanychbek@gmail.com>
2019-09-26 18:54:03 +0000

    style control buttons

--- src/controls.js
+++ src/controls.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Controls({turnLeft, turnRight, move}) {
   return (
     <div>
-      <button onClick={turnLeft}>turn left</button>
+      <Button onClick={turnLeft} color="royalblue">turn left</Button>
       &nbsp;
-      <button onClick={turnRight}>turn right</button>
+      <Button onClick={turnRight} color="royalblue">turn right</Button>
       &emsp;
-      <button onClick={move} disabled={move == null}>step forward</button>
+      <Button onClick={move} disabled={move == null} color="mediumseagreen">step forward</Button>
     </div>
   )
 }
 
 export {Controls}
+
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  background: none;
+  color: ${props => props.disabled ? 'gray' : props.color};
+  border: 2px solid ${props => props.disabled ? 'gray' : props.color};
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
+`
